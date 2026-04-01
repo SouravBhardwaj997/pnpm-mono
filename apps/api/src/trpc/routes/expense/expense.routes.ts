3bd6f4ab37d2edb4566ab5087356e8e4af3c2871
Sourav Bhardwaj <Souravbhardwaj997@gmail.com>
2026-04-01 17:34:43 +0530

    feat: add expense router

--- apps/api/src/trpc/routes/expense/expense.routes.ts
+++ apps/api/src/trpc/routes/expense/expense.routes.ts
@@ -1,10 +1,10 @@
 import { prisma } from "@/lib/prisma";
 import { protectedProcedure, router } from "@/trpc/trpc";
-import { expenseListInputSchema } from "./schema";
+import { addUserExpenses, getUserExpensesInputSchema } from "./schema";
 
 export const expenseRouter = router({
-  expenseList: protectedProcedure
-    .input(expenseListInputSchema)
+  getUserExpenses: protectedProcedure
+    .input(getUserExpensesInputSchema)
     .query(async ({ input, ctx }) => {
       const { limit, cursor } = input;
       const expenses = await prisma.expense.findMany({ where: { userId: ctx.user.id }, take: Number(limit) + 1, ...(cursor && { cursor: { id: Number(cursor) }, skip: 1 }), orderBy: {
@@ -15,4 +15,11 @@ export const expenseRouter = router({
       const nextCursor = hasMore ? data.at(-1)?.id : null;
       return { expenses: data, nextCursor, hasMore };
     }),
+  addUserExpenses: protectedProcedure.input(addUserExpenses).query(async ({ input, ctx }) => {
+    const { amount, categoryId, note, paymentMethod } = input;
+    const expense = await prisma.expense.create({ data: { amount, note, paymentMethod, categoryId, userId: ctx.user.id } });
+    return {
+      expense,
+    };
+  }),
 });
--- apps/api/src/trpc/routes/expense/schema.ts
+++ apps/api/src/trpc/routes/expense/schema.ts
@@ -1,6 +1,13 @@
 import z from "zod";
 
-export const expenseListInputSchema = z.object({
+export const getUserExpensesInputSchema = z.object({
   cursor: z.string().optional(),
   limit: z.string(),
+});
+
+export const addUserExpenses = z.object({
+  amount: z.int().min(1, "Amount is required"),
+  note: z.string().min(1, "Note is required"),
+  paymentMethod: z.enum(["CARD", "CASH"]).default("CARD"),
+  categoryId: z.int().min(1, "Category ID is required"),
 });
